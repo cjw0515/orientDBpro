1b567704a38cd7b094673fadc875ca656d09f515
jw <cjw0515@naver.com>
2017-11-28 23:58:20 +0900

    글 보기/생성 코드 추가

--- app_orientdb.js
+++ app_orientdb.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('views','./views_orientdb');
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //==================db연결부분==========================
-//깃 테스트
 var OrientDB = require('orientjs');
 
 var server = OrientDB({
@@ -15,22 +17,65 @@ var server = OrientDB({
   password:'root'
 });
 var db = server.use('o2');
+/*
 db.record.get('#26:0').then(function(record){
   console.log('loaded record:', record);
 });
+*/
 //============================================
+//라우트의 순서는 경우에따라서 중요할 수 있다.
+app.get('/topic/add',function(req, res){
+  var sql = 'select * from topic';
+  db.query(sql).then(function(topics){
+    if(topics.length === 0){ //결과값은 배열이기때문에
+      console.log('there is no record.');
+      res.status(500).send('Internal Serve Error');
+    }
+      res.render('add',{topics:topics});
+  });
+});
 
 app.get(['/topic','/topic/:id'],function(req, res){
   var sql = 'select from topic';
+
   db.query(sql).then(function(topics){
-    res.render('view',{topics:topics});//정보전달
+    var id = req.params.id;
+    //아이디값이 있다면 파라미터를 2개 넘겨줌.
+    if(id){
+      var sql = 'select * from topic where @rid=:rid';
+      var param = {
+        params:{
+          rid:id
+        }
+      };
+      db.query(sql,param).then(function(topic){
+        res.render('view',{topics:topics,topic:topic[0]})
+      });
+    }else{
+        res.render('view',{topics:topics});//정보전달
+    }
+  });
+});
+
+app.post('/topic/add',function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'insert into topic (title, description, author)values(:title, :desc, :author)';
+  var param = {
+    params:{
+      title:title,
+      desc:description,
+      author:author
+    }
+  }
+  db.query(sql,param).then(function(results){
+    //res.send(results[0]['@rid']);
+    res.redirect('/topic/'+ encodeURIComponent(results[0]['@rid']));
   });
 
 
-
-})
-
-
+});
 
 
 
